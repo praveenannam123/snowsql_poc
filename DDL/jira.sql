8f328866fc0d0961f12c41ef6966a8a782f7c3c3
praveenannam123 <139906279+praveenannam123@users.noreply.github.com>
2023-08-09 15:55:06 -0500

    Delete DDL directory

--- DDL/jira.sql
+++
@@ -1,6 +0,0 @@
-create or replace table raw.chandra_code_dev
-(abc char(10),
-  xyz char(20) ,
-  wer char(20),
-  pqr char(20)
-  )
--- DDL/jira1.sql
+++
@@ -1,5 +0,0 @@
-create or replace table raw.code_dev
-(abc char(20),
-  xyz char(20) ,
-  wer char(10)
-  )
